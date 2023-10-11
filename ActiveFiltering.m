40c93650911a9ea24ef7f35e0c34b72feb1b0874
Thomas Parr <t.parr@stanhopeai.com>
2023-10-11 20:48:29 +0100

    Initial commit

---
+++ ActiveFiltering.m
@@ -0,0 +1,206 @@
+function [Y,M,t,a] = ActiveFiltering(f,F,Pf,g,G,Pg,x0,m0,n,s,dt,T,a0,y,yc)
+% Generates a data timeseries Y and belief timeseries M with times t
+%
+% f, F       are the flow functions for model and process
+% g, G       are the expectations from the likelihood (as above)
+% x0, m0     are the initial conditions for states and beliefs
+% Pf, Pg     are precisions for the flow, likelihood, and priors
+% n          gives the order of motion to be used
+% s          gives the assumed smoothness of the noise process (for model only)
+% dt         is the timestep to be used
+% T          is the duration of the simulation
+% a0         is the initial active state
+% y          are data (if not expected to be simulated)
+% yc         indexes those data modalities that are controllable
+%__________________________________________________________________________
+
+try T = dt*(size(y,2)-1); catch,                 end
+try Ng = size(g(x0),1);   catch, Ng = size(g,1); end
+
+t = 0:dt:T;
+
+Nf = size(x0,1);
+
+g0 = cell(n,1);
+G0 = cell(n,1);
+f0 = cell(n,1);
+F0 = cell(n,1);
+
+% Initialise generalised coordinates of motion for generative process and
+% model
+%--------------------------------------------------------------------------
+X     = cell(n,1);
+Y     = cell(n,1);
+M     = cell(n,1);
+X{1}  = zeros(Nf,length(t)); X{1}(:,1) = x0;
+M{1}  = zeros(Nf,length(t)); M{1}(:,1) = m0;
+
+if ~isempty(a0)
+    a    = zeros(1,length(t));
+    a(1) = a0;
+end
+
+try Y{1} = y; Y{1}(yc,:) = 0; catch, Y{1} = zeros(Ng,length(t)); end
+try f0{1} = f(M{1}(:,1));     catch, f0{1} = f;   end
+fJ = AF_dfdx(f,M{1}(:,1));
+
+for j = 2:n
+    X{j}     = zeros(Nf,length(t));
+    if exist('y','var')
+        Y{j} = gradient(Y{j-1})/dt;
+        Y{j}(yc,:) = 0;
+    else
+        Y{j} = zeros(Ng,length(t));
+    end
+    M{j}     = zeros(Nf,length(t)); M{j}(:,1) = f0{j-1};
+    f0{j}    = fJ*M{j}(:,1);
+end
+f0{end} = zeros(size(f0{end}));
+
+% Initialise free energy gradients and Hessian
+%--------------------------------------------------------------------------
+
+% Initialise autocorrelations 
+%--------------------------------------------------------------------------
+A          = zeros(n,n);
+k          = 0:(n-1);
+x          = sqrt(2)*s;
+r(1+2*k)   = cumprod(1-2*k)./(x.^(2*k));
+
+for z = 1:n
+    A(:,z) = r((1:n) + z - 1);
+    r      = -r;
+end
+
+% Generalised precisions
+%--------------------------------------------------------------------------
+Pg = (pinv(kron(A,pinv(Pg))));
+Pf = (pinv(kron(A,pinv(Pf))));
+D  = eye(n);
+D  = [D(2:n,:);zeros(1,n)];
+D  = sparse(kron(D,eye(Nf)));
+
+for i = 1:length(t)
+
+    % Derivatives for likelihood and flow functions
+    %----------------------------------------------------------------------
+
+    % First order of motion
+    %----------------------------------------------------------------------
+    g0{1} = g(M{1}(:,i));         G0{1} = G(X{1}(:,i));
+    gJ    = AF_dfdx(g,M{1}(:,i)); GJ    = AF_dfdx(G,X{1}(:,i));
+    f0{1} = f(M{1}(:,i));
+    fJ    = AF_dfdx(f,M{1}(:,i));
+    if isempty(a0)
+        F0{1} = F(X{1}(:,i));
+        FJ    = AF_dfdx(F,X{1}(:,i));
+    else
+        F0{1} = F(X{1}(:,i),a(i));
+        FJ    = AF_dfdx(F,X{1}(:,i),a(i),1);
+        FJa   = AF_dfdx(F,X{1}(:,i),a(i),2);
+    end
+    
+    % Subsequent orders of motion
+    %----------------------------------------------------------------------
+    for k = 2:n
+        X{k}(:,i) = F0{k-1};
+        G0{k} = GJ*X{k}(:,i);
+        g0{k} = gJ*M{k}(:,i);
+        f0{k} = fJ*M{k}(:,i);
+        F0{k} = FJ*X{k}(:,i);
+    end
+    f0{end} = zeros(size(f0{end}));
+
+    % Generate data and higher order generalised states [noise free for now]
+    %----------------------------------------------------------------------
+    for k = 1:(n-1)
+        if ~exist('y','var'), Y{k}(:,i)   = G0{k};
+        else Y{k}(yc,i)                   = G0{k}(yc);
+        end
+
+    end
+    if ~exist('y','var'), Y{n}(:,i) = G0{n};
+    else Y{n}(yc,i)                 = G0{n}(yc);
+    end
+    
+    if i == length(t)
+        return
+    end
+
+    % Free energy gradients and Hessian
+    %----------------------------------------------------------------------
+    yY = zeros(Ng*n,1);
+    m  = zeros(Nf*n,1);
+    for j = 1:n
+        yY((j-1)*Ng + (1:Ng),:) = Y{j}(:,i);
+        m((j-1)*Nf + (1:Nf),:)  = M{j}(:,i);
+    end
+    
+    DfJ   = kron(eye(n),fJ);
+    DgJ   = kron(eye(n),gJ);
+    dFdx  =  (DfJ - D)'*Pf*(D*m - cat(1,f0{:}))  + DgJ'*Pg*(yY - cat(1,g0{:}));
+    dFdxx = -(DfJ - D)'*Pf*(DfJ - D) - DgJ'*Pg*DgJ;
+
+    % Update beliefs
+    %----------------------------------------------------------------------
+    m = m + pinv(dFdxx + D)*(expm(dt*(dFdxx + D)) - eye(Nf*n))*(dFdx + D*m);
+
+    for k = 1:n
+        M{k}(:,i+1) = m((k-1)*Nf + (1:Nf),:);
+    end
+    
+    % And action
+    %----------------------------------------------------------------------
+    if ~isempty(a0)
+        dyda   = zeros(Ng*n,length(a0));
+        for k = 2:n
+            dyda((Ng*(k-1)) + (1:Ng),:)   = GJ*(FJ^(k-2))*FJa;
+        end
+        dFda   = - dyda'*Pg*(yY - cat(1,g0{:}));
+        dFdaa  = - dyda'*Pg*dyda;
+        dFdax  = - dyda'*Pg*kron(ones(n,1),GJ);
+    end
+
+    % Update action and states
+    %----------------------------------------------------------------------
+    if isempty(a0)
+        X{1}(:,i+1) = X{1}(:,i) + (expm(dt*FJ) - eye(Nf))*pinv(FJ)*F0{1};
+    else
+        Jxa = [FJ FJa;dFdax dFdaa];
+        Xa          = [X{1}(:,i); a(:,i)] + pinv(Jxa)*(expm(dt*Jxa) - eye(size(Jxa,1)))*[F0{1};dFda];
+        X{1}(:,i+1) = Xa(1:Nf,1);
+        a(:,i+1)    = Xa(Nf+1:end,1);
+    end
+end
+
+function J = AF_dfdx(f,x,a,n)
+% Numerical derivative with respect to argument n using finite difference method
+
+if nargin>2
+    if n == 1
+        J = zeros(size(f(x,a),1),size(x,1));
+
+        for i = 1:size(x,1)
+            dx     = zeros(size(x));
+            dx(i)  = exp(-4);
+            J(:,i) = (f(x+dx,a) - f(x-dx,a))/(2*exp(-4));
+        end
+    elseif n == 2
+        J = zeros(size(f(x,a),1),size(a,1));
+
+        for i = 1:size(a,1)
+            da     = zeros(size(a));
+            da(i)  = exp(-4);
+            J(:,i) = (f(x,a+da) - f(x,a-da))/(2*exp(-4));
+        end
+    end
+
+else
+    J = zeros(size(f(x),1),size(x,1));
+
+    for i = 1:size(x,1)
+        dx     = zeros(size(x));
+        dx(i)  = exp(-4);
+        J(:,i) = (f(x+dx) - f(x-dx))/(2*exp(-4));
+    end
+end
---
+++ MetronomeModel.m
@@ -0,0 +1,192 @@
+function [Y,M,t,a] = MetronomeModel(OPTIONS,P,y)
+% This function specifies and inverts the generative model for our 
+% metronome task, taking the parameters of our model and optionally data y
+% and returning outstanding simulated data Y, our belief trajectory M,
+% timesteps t, and action a. OPTIONS inlcludes option for plotting.
+%
+% Thomas Parr
+%--------------------------------------------------------------------------
+
+% Preliminaries
+%--------------------------------------------------------------------------
+No   = 4;               % Number of clocks
+Nt   = 2056;            % Number of timesteps
+dt   = 1/4;             % Length of timestep
+tT   = (0:Nt)*dt;       % Time
+T    = round(tT(end));  % Final time
+
+%% Generative model
+%-------------------------------------------------------------------------- 
+
+% Equations of motion
+%--------------------------------------------------------------------------
+
+% Jacobian for pendular dynamics
+%--------------------------------------------------------------------------
+for i = 1:2:2*No 
+    Jf(i:i+1,i:i+1) = [0  i;
+                      -i  0]/32;
+end
+
+% Transition dynamics
+%--------------------------------------------------------------------------
+b11  = 0; b22 = 0;
+B    = [b11   1-b22;
+       1-b11   b22];
+
+% Attractor locations
+%--------------------------------------------------------------------------
+tgt = [-1 1];
+
+% Combine pendular dynamics with transition and attractor dynamics
+%--------------------------------------------------------------------------
+
+smax = @(x) exp(x)/sum(exp(x));             % Softmax function
+sig  = @(x) 1/(1+exp(-4*x));                % Sigmoid function
+try gamma = P.gamma; catch, gamma = 2; end  % Precision of policy selection
+
+f = @(x) [Jf*x(1:size(Jf,2),1);                                                                 % oscillators as above
+          sig(sum(x(1:2:size(Jf,2)))-2)*(smax(gamma*x(end-2:end-1,1))-x(end-4:end-3,1))/4;      % current state
+          (1-sig(sum(x(1:2:size(Jf,2)))))*(log(B*x(end-4:end-3,1)+exp(-2))-x(end-2:end-1,1))/4; % next state
+          (tgt*x(end-4:end-3,1) - x(end))/4];                                                   % attractor dynamics
+
+% Prediction of data (i.e., mode of likelihood)
+%--------------------------------------------------------------------------
+g = @(x) [exp(sum(x(1:2:end-5)))/64; x(end)];
+
+% Precisions and orders of motion
+%--------------------------------------------------------------------------
+Pg      = diag([exp(2) exp(0)]); % Precision of likelihood
+Pf      = exp(4)*eye(2*No+5);    % Precision of dynamics
+Pf(end) = exp(0);                % Less precise controllable dynamics
+n       = 3;                     % Order of generalised motion
+s       = 1;                     % Smoothness
+m0      = randn(2*No + 5,1)/16;  % Initial beliefs
+m0(end-4:end-1) = [1;0;0;1];
+
+%% Generative process
+%--------------------------------------------------------------------------
+x0      = zeros(2*No + 5,1);     % Initial states (for generating data)
+a0      = 0;                     % Initial active states
+
+try   x0(2*OPTIONS.freq) = 4;    % Option to set pendulum frequency
+catch, x0(4)   = 4; end          % Non-zero amplitude for second pendulum
+
+
+% Equations of motion
+%--------------------------------------------------------------------------
+F = @(x,a) [Jf*x(1:size(Jf,2),1);                                                     % oscillators as above
+            sig(sum(x(1:2:size(Jf,2)))-2)*(x(end-2:end-1,1)-x(end-4:end-3,1))/4;      % current state
+            (1-sig(sum(x(1:2:size(Jf,2)))))*(B*x(end-4:end-3,1)-x(end-2:end-1,1))/4;  % next state
+            a];                                                                       % attractor dynamics
+
+%% Model inversion
+%--------------------------------------------------------------------------
+
+if exist('y','var')
+    [Y,M,t,a] = ActiveFiltering(f,F,Pf,g,g,Pg,x0,m0,n,s,dt,T,a0,y,2);
+else
+    [Y,M,t,a] = ActiveFiltering(f,F,Pf,g,g,Pg,x0,m0,n,s,dt,T,a0);
+end
+
+if OPTIONS.plot == 0, return, end
+
+% Plotting
+%--------------------------------------------------------------------------
+% The first plot gives a summary of the behaviour of the variables in the
+% model. These include the data presented to our agent and its predictions
+% of these data (dashed) in the upper plot, beliefs about the trajectory of
+% (imaginary) oscillators in the secon plot, beliefs involved in sequencing
+% action in the third plot, and action in the final plot.
+
+figure('Name','Variable Trajectories','Color','w')
+
+subplot(4,1,1)
+plot(t,Y{1}), hold on
+for i = 1:size(M{1},2)
+    pY(:,i) = g(M{1}(:,i));
+    pF(:,i) = [sig(sum(M{1}((1:2:(size(M{1},1)-4)),i))-2);(1-sig(sum(M{1}(1:2:(size(M{1},1)-4),i))))];
+end
+plot(t,pY,'--k'), hold off
+title('Data (-) and predictions (--)')
+axis tight
+
+subplot(4,1,2)
+plot(t,M{1}(1:end-4,:))
+title('Beliefs (modes) about oscillators')
+axis tight
+
+subplot(4,1,3)
+C = zeros(16,length(t),3);
+C(:,:,1) = repmat(1-pF(1,:)/4,16,1,1);
+C(:,:,2) = repmat(1-pF(2,:)/4 -pF(1,:)/4,16,1,1);
+C(:,:,3) = repmat(1-pF(2,:)/4,16,1,1);
+image([t(1) t(end)],[-2 2],C), hold on
+plot(t,M{1}(end-4:end-1,:)), hold off
+title('Beliefs about sequencing')
+
+subplot(4,1,4)
+plot(t,a)
+title('Action')
+axis tight
+
+% Our second figure provides an animated version of the above designed to
+% offer some intuition as to the simulated performance of the task and the
+% belief mechanics that underwrite this.
+
+figure('Name','Animation','Color','w')
+
+[i,j] = meshgrid(-10:0.1:10,-10:0.1:10);
+stim  = exp(-(i.^2 + j.^2)/4);
+
+for k = 1:16:size(Y{1},2)
+    subplot(3,2,1)
+    imagesc(-stim*Y{1}(1,k)), colormap gray, clim([-max(Y{1}(1,:)) 0])
+    axis square, axis off
+    title('Stimulus')
+
+    subplot(3,2,2)
+    for i = 1:2:2*No
+        plot([0 M{1}(i,k)],[0 M{1}(i+1,k)],'k'), hold on
+        plot(M{1}(i,k),M{1}(i+1,k),'.k','MarkerSize',8)
+    end
+    hold off
+    xlim([-max(max(M{1}(1:2*No,:))) max(max(M{1}(1:2*No,:)))])
+    ylim([-max(max(M{1}(1:2*No,:))) max(max(M{1}(1:2*No,:)))])
+    axis square
+    axis off
+    title('Internal clock')
+
+    subplot(3,1,2)
+    plot(tgt,[0 0],'.r','MarkerSize',32), hold on
+    if k > 8
+        for j = 8:-1:0
+            plot(Y{1}(2,k-j),0,'.','Color',[1 1 1]*j/8,'MarkerSize',16)
+        end
+    end
+    hold off
+    xlim([-1 1] + tgt)
+    axis off
+    title('Behaviour')
+
+    subplot(3,2,5)
+    imagesc(1-M{1}(2*No+(1:2),k)'), colormap gray, axis off, clim([0 1])
+    title('Current target')
+
+    subplot(3,2,6)
+    imagesc(1-smax(M{1}(2*No+(3:4),k))'), colormap gray, axis off, clim([0 1])
+    title('Planned next target')
+
+    drawnow
+
+    % Animation
+    %----------------------------------------------------------------------
+    % F  = getframe(gcf);
+    % im = frame2im(F);
+    % [MM,MMM] = rgb2ind(im,256);
+    % if k==1
+    %     imwrite(MM,MMM,'C:\Users\tparr\Dropbox\Notes on papers\Notes\Sanjay Manohar\TP\Animation.gif','gif','LoopCount',Inf,'DelayTime',0.1);
+    % else
+    %     imwrite(MM,MMM,'C:\Users\tparr\Dropbox\Notes on papers\Notes\Sanjay Manohar\TP\Animation.gif','gif','WriteMode','append','DelayTime',0.1);
+    % end
+end
