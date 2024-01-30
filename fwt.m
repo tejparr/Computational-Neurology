9882ae2e194cbc71341708ca5f3173b91e0eabdd
Thomas Parr <thomas.parr@ndcn.ox.ac.uk>
2024-01-30 17:29:27 +0000

    Minor updates to metronome model and additional comments

--- fwt.m
+++ fwt.m
@@ -1,4 +1,5 @@
 function S = fwt(X,f,w,dt)
+% Format S = fwt(X,f,w,dt)
 % Time-freq analysis using a Fourier Wavelet Transform of X (an n x t 
 % matrix) with frequencies given by the vector f and window given by w.
 %--------------------------------------------------------------------------
