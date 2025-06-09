a09b34dbabb410d0b56e50218c16210f30c3a06e
Thomas Parr <thomas.parr@ndcn.ox.ac.uk>
2025-06-09 14:56:12 +0100

    Minor correction

--- mp_POMDP.m
+++ mp_POMDP.m
@@ -279,10 +279,12 @@ for t = 1:T
         pomdp.h = mp_pomdp_h(pomdp.h,D);
         if isfield(pomdp,'hstop') && pomdp.hstop
             if all(cellfun(@isempty,pomdp.h))
-                pomdp.T = t;
-                pomdp.Q = pomdp.Q{1:t,:};
-                pomdp.P = pomdp.P{1:t,:};
-                u       = u(:,1:t);
+                pomdp.T = min(t+1,pomdp.T);   
+            end
+            if t == pomdp.T
+                pomdp.Q = pomdp.Q{1:t+1,:};
+                pomdp.P = pomdp.P{1:t+1,:};
+                u       = u(:,1:t+1);
                 break
             end
         end
